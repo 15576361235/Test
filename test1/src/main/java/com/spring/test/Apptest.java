0842959798d26ea999361af0dce382db1219ea68
xieqi <465385335@qq.com>
2018-03-22 22:49:56 +0800

    11111

---
+++ test1/src/main/java/com/spring/test/Apptest.java
@@ -0,0 +1,15 @@
+package com.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class Apptest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apptest.class, args);
+
+	}
+
+}
