bcc8aa7cbc026820d1f3b4ce37489cc2e6c4e2da
xieqi <465385335@qq.com>
2018-03-22 22:58:53 +0800

    111

--- test1/src/main/java/com/spring/test/Apptest.java
+++ test1/src/main/java/com/spring/test/Apptest.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Apptest {
 
 	public static void main(String[] args) {
+		System.out.println("111");
 		SpringApplication.run(Apptest.class, args);
 
 	}
